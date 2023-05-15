4dffc4b2bc8a306fdcfdeaea471153d129bbcbdc
Ivan Ponamarev <ponamarevrabota@gmail.com>
2023-05-15 07:00:57 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Иван Понамарев on 15.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
